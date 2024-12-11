1e41b150212a7c3d1497903e6f5adce4b487b26f
Su-zenGeurtsPXL <suzen.geurts@student.pxl.be>
2024-12-11 14:20:04 +0100

    [US1] #14 Updated tests for post-item component

--- frontend-web/news-app/src/app/core/posts/post-item/post-item.component.spec.ts
+++ frontend-web/news-app/src/app/core/posts/post-item/post-item.component.spec.ts
@@ -6,34 +6,26 @@ import {ComponentFixture, TestBed} from "@angular/core/testing";
 import {provideRouter} from "@angular/router";
 import {NO_ERRORS_SCHEMA} from "@angular/core";
 import {By} from "@angular/platform-browser";
-import { provideHttpClient, provideHttpClientTesting, HttpTestingController } from "@angular/common/http/testing";
 
 describe('PostItemComponent', () => {
   let component: PostItemComponent;
   let fixture: ComponentFixture<PostItemComponent>;
-  let httpTestingController: HttpTestingController;
-
+  let postServiceMock: jasmine.SpyObj<PostService>;
   const mockPost: Post = new Post(1, 'Title1', 'Content...', 1, 'ACADEMIC', '2024-12-10 15:30:07', 'PUBLISHED');
 
   beforeEach(() => {
-    // postServiceMock = jasmine.createSpyObj('PostService', ['toPascalCasing', 'transformDate']);
+    postServiceMock = jasmine.createSpyObj('PostService', ['toPascalCasing', 'transformDate']);
     TestBed.configureTestingModule({
       imports: [PostItemComponent],
       providers: [
         provideRouter([]),
-        provideHttpClient(),
-        provideHttpClientTesting(),
+        { provide: PostService, useValue: postServiceMock },
       ],
       schemas: [NO_ERRORS_SCHEMA]
     });
 
     fixture = TestBed.createComponent(PostItemComponent);
     component = fixture.componentInstance;
-    httpTestingController = TestBed.inject(HttpTestingController);
-  });
-
-  afterEach(() => {
-    httpTestingController.verify(); // Ensure no outstanding HTTP requests
   });
 
   it('should create the component', () => {
@@ -47,5 +39,4 @@ describe('PostItemComponent', () => {
     const debugElement = fixture.debugElement.query(By.css('h1'));
     expect(debugElement.nativeElement.textContent).toContain('Title1');
   });
-
 });
