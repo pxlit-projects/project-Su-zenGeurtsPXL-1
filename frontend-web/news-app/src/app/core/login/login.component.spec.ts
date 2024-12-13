964215f7a58a927748e252e7db8ca49729be6161
Su-zenGeurtsPXL <suzen.geurts@student.pxl.be>
2024-12-14 00:48:49 +0100

    [US1] #14 Added tests for login component

---
+++ frontend-web/news-app/src/app/core/login/login.component.spec.ts
@@ -0,0 +1,111 @@
+import {User} from "../../shared/models/user.model";
+import {UserRequest} from "../../shared/models/user-request.model";
+import {AuthenticationService} from "../../shared/services/authentication.service";
+import {LoginComponent} from "./login.component";
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+  let authenticationServiceMock: jasmine.SpyObj<AuthenticationService>;
+  let routerMock: jasmine.SpyObj<Router>;
+
+  beforeEach(() => {
+    localStorage.setItem('userId', '1');
+    authenticationServiceMock = jasmine.createSpyObj('AuthenticationService', ['login', 'getUserById']);
+    routerMock = jasmine.createSpyObj('Router', ['navigate', 'then']);
+
+    TestBed.configureTestingModule({
+      imports: [LoginComponent, ReactiveFormsModule],
+      providers: [
+        { provide: AuthenticationService, useValue: authenticationServiceMock },
+        { provide: Router, useValue: routerMock }
+      ]
+    });
+
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should call login on form submit for editor and navigate on success', () => {
+    const userRequest = {
+      username: 'john',
+      password: '123'
+    };
+
+    const mockUser = {
+      id: 1,
+      username: 'john',
+      fullName: 'John Doe',
+      password: '123',
+      role: 'editor'
+    };
+
+    component.loginForm.setValue(userRequest);
+
+    authenticationServiceMock.login.and.returnValue(mockUser as User);
+
+    component.onSubmit();
+
+    expect(authenticationServiceMock.login).toHaveBeenCalledWith(userRequest as UserRequest);
+
+    expect(component.loginForm.pristine).toBeTrue();
+    expect(routerMock.navigate).toHaveBeenCalledWith(['/myPost']);
+  });
+
+  it('should call login on form submit for user and navigate on success 2', () => {
+    const userRequest = {
+      username: 'john',
+      password: '123'
+    };
+
+    const mockUser = {
+      id: 1,
+      username: 'john',
+      fullName: 'John Doe',
+      password: 'ed123',
+      role: 'user'
+    };
+
+    component.loginForm.setValue(userRequest);
+
+    authenticationServiceMock.login.and.returnValue(mockUser as User);
+
+    component.onSubmit();
+
+    expect(authenticationServiceMock.login).toHaveBeenCalledWith(userRequest as UserRequest);
+
+    expect(component.loginForm.pristine).toBeTrue();
+    expect(routerMock.navigate).toHaveBeenCalledWith(['/post']);
+  });
+
+  it('should set invalidLoginForm to true when form is invalid', () => {
+     component.onSubmit();
+    expect(component.invalidLoginForm).toBeTrue();
+  });
+
+  it('should set invalidLoginForm to true when form is invalid', () => {
+    const userRequest = {
+      username: 'john',
+      password: 'invalidPassword'
+    };
+
+    const mockUser = null;
+
+    component.loginForm.setValue(userRequest);
+
+    authenticationServiceMock.login.and.returnValue(mockUser);
+
+    component.onSubmit();
+
+    expect(authenticationServiceMock.login).toHaveBeenCalledWith(userRequest as UserRequest);
+
+    expect(component.loginForm.pristine).toBeTrue();
+    expect(component.invalidLogin ).toBeTrue();
+  });
+
+});
