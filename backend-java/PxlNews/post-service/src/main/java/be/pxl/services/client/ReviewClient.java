9061adb81e16bddf499d37f08dfd97852479137d
Su-zenGeurtsPXL <suzen.geurts@student.pxl.be>
2025-01-02 20:15:20 +0100

    [Fix] Updated Review client with headers

--- backend-java/PxlNews/post-service/src/main/java/be/pxl/services/client/ReviewClient.java
+++ backend-java/PxlNews/post-service/src/main/java/be/pxl/services/client/ReviewClient.java
@@ -4,11 +4,12 @@ import be.pxl.services.domain.Review;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.List;
 
 @FeignClient(name = "review-service")
 public interface ReviewClient {
     @GetMapping(path = "/api/review/post/{postId}")
-    List<Review> getReviewsByPostId(@PathVariable Long postId);
+    List<Review> getReviewsByPostId(@PathVariable Long postId, @RequestHeader String userRole);
 }
