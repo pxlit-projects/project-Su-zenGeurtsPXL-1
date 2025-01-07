7aba7fb5ade1d87b2e39fa89cdc91d74f2e16f61
Su-zenGeurtsPXL <suzen.geurts@student.pxl.be>
2025-01-07 21:57:29 +0100

    [FIX] Solved glitching of the navbar and added notification badge

--- frontend-web/news-app/src/app/core/navbar/navbar.component.ts
+++ frontend-web/news-app/src/app/core/navbar/navbar.component.ts
@@ -1,64 +1,42 @@
-import {Component, inject, OnDestroy, OnInit} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {Router, RouterLink, RouterLinkActive} from "@angular/router";
 import {AsyncPipe, NgOptimizedImage} from "@angular/common";
 import {AuthenticationService} from "../../shared/services/authentication/authentication.service";
 import {PostService} from "../../shared/services/post/post.service";
 import {Notification} from "../../shared/models/posts/notification.model";
-import {interval, Observable, Subscription} from "rxjs";
-import {PostItemComponent} from "../posts/post-item/post-item.component";
+import {interval, tap} from "rxjs";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, NgOptimizedImage, AsyncPipe, PostItemComponent],
+  imports: [RouterLink, RouterLinkActive, NgOptimizedImage, AsyncPipe],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent implements OnInit, OnDestroy{
+export class NavbarComponent implements OnInit {
   protected readonly localStorage = localStorage;
   router: Router = inject(Router);
   authenticationService: AuthenticationService = inject(AuthenticationService);
   postService: PostService = inject(PostService);
   userMenuIsHidden: boolean = true;
   notificationsMenuIsHidden: boolean = true;
-  notifications$!: Observable<Notification[]>;
+  notifications$: Notification[] | undefined;
   hasNoNotifications: boolean = true;
-
-  private fetchSubscription: Subscription | undefined;
 
   ngOnInit(): void {
     this.fetchNotifications();
-    this.fetchSubscription = interval(5000).subscribe(() => this.fetchNotifications());
-  }
-
-  ngOnDestroy(): void {
-    this.fetchSubscription?.unsubscribe();
+    interval(1000).subscribe(() => this.fetchNotifications());
   }
 
   fetchNotifications() {
-    if (localStorage.getItem('userRole') === 'editor') {
-      let fetchedNotifications = this.postService.getMyNotifications();
-      if (this.notifications$ === undefined) {
-        this.handleFetch(fetchedNotifications);
-      } else {
-        fetchedNotifications.subscribe(fetchedNotificationsData => {
-          this.notifications$.subscribe(notifications => {
-            if (notifications != fetchedNotificationsData) {
-              this.handleFetch(fetchedNotifications);
-            }
-          });
-        });
-      }
-    }
+    if (localStorage.getItem('userRole') !== 'editor') return;
+    this.postService.getMyNotifications()
+      .pipe(tap())
+      .subscribe(notifications => {
+        this.notifications$ = notifications.filter(notification => !notification.isRead);
+        this.hasNoNotifications = this.notifications$.length == 0;
+      });
   }
-
-  handleFetch(data: Observable<Notification[]>) {
-    this.notifications$ = data;
-    this.notifications$.subscribe(notifications => {
-      const unreadNotifications = notifications.filter(notification => !notification.isRead)
-      this.hasNoNotifications = unreadNotifications.length == 0;
-    });
-}
 
   userMenu() {
     this.userMenuIsHidden = !this.userMenuIsHidden;
